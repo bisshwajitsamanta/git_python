a51814ad6b5b242704e491e70ebf5c8c687b8c34
Samanta <bsamanta@volvocars.com>
2024-08-16 21:20:41 +0530

    Modified safe directory

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 import sys
 from os import getenv
@@ -14,14 +15,15 @@ def run_command(command):
 def main():
     create_orphan_branch = getenv('INPUT_CREATE_ORPHAN_BRANCH', 'false')
     branch = getenv('INPUT_BRANCH', 'deployment')
-    user_name = getenv('INPUT_USER_NAME', 'Github Actions')
-    user_email = getenv('INPUT_USER_EMAIL', 'actions@github.com')
+    user_name = getenv('INPUT_CONFIG_USER', 'Github Actions')
+    user_email = getenv('INPUT_CONFIG_EMAIL', 'actions@github.com')
 
     if create_orphan_branch.lower() == 'true':
+        run_command(f'git config --global --add safe.directory {os.getcwd()}')
         run_command(f'git switch --orphan {branch}')
         run_command('git config --local push.default current')
-        run_command(f'git config --local user.name "{user_name}')
-        run_command(f'git config --local user.email "{user_email}')
+        run_command(f'git config --local user.name "{user_name}"')
+        run_command(f'git config --local user.email "{user_email}"')
         run_command('git commit --allow-empty -m "Initial Deployment Branch commit"')
         run_command(f'git push origin {branch}')
 
